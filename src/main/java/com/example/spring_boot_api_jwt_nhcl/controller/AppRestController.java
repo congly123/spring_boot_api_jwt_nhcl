84ffb849cfac5c6e6b32aaa8624e06be0e6156d0
Administrator <nguyenhuynhcongly17092000@gmail.com>
2021-09-26 13:23:19 +0700

    branch_register

--- src/main/java/com/example/spring_boot_api_jwt_nhcl/controller/AppRestController.java
+++ src/main/java/com/example/spring_boot_api_jwt_nhcl/controller/AppRestController.java
@@ -10,5 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppRestController {
-   
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/register")
+    public String saveUserRegister(@RequestBody User user){
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        if(userService.saveAndFlush(user)){
+            return "Register Successful!";
+        }
+        return "Register Fail!";
+    }
 }
